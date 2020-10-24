b124f03b967f900419d690610201d86bf84125a4
Sanjay567-coder <72558978+Sanjay567-coder@users.noreply.github.com>
2020-10-24 11:05:01 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -27,15 +27,6 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
-	rectangle1 = createSprite(400,650,200,20);
-	rectangle1.shapeColor = "red";
-
-	rectangle2 = createSprite(295,610,20,100);
-	rectangle2.shapeColor = "red";
-
-	rectangle3 = createSprite(505,610,20,100);
-	rectangle3.shapeColor = "red";
-
 	engine = Engine.create();
 	world = engine.world;
 
@@ -45,16 +36,28 @@ function setup() {
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
+	 World.add(world, ground);
 
-	 rectangle1 = Bodies.rectangle(400,650,200,20,{isStatic:true} );
-	 World.add(world, rectangle1);
+	rectPos = width/2-1000;
+	rectY = 605;
+	 
+     rectangle1 = createSprite(rectPos+100, rectY+40, 200,20);
+	 rectangle1.shapeColor = "red";
 
-	 rectangle2 = Bodies.rectangle(295,610,20,100,{isStatic:true} );
-	 World.add(world, rectangle2);
+	 bottomRect = Bodies.rectangle(rectPos+100, rectY+45-20, 200,20 , {isStatic:true} );
+	 World.add(world, bottomRect);
 
-	 rectangle3 = Bodies.rectangle(505,610,20,100,{isStatic:true} );
-	 World.add(world, rectangle3);
+	 rectangle2 = createSprite(rectPos, rectY, 20,100);
+	 rectangle2.shapeColor = "red";
+
+	 leftRect = Bodies.rectangle(rectPos+20, rectY, 20,100 , {isStatic:true} );
+	 World.add(world, leftRect);
+
+	 rectangle3 = createSprite(rectPos+200 , rectY, 20,100);
+	 rectangle3.shapeColor = "red";
+
+	 rightRect = Bodies.rectangle(rectPos+200-20 , rectY, 20,100 , {isStatic:true} );
+	 World.add(world, rightRect);
 	  
 	Engine.run(engine);
   
